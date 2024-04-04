147af9c1be23fea551c79af4ec14194dfe03d7cd
mitifromqt <minhtien2812000@gmail.com>
2024-04-04 10:05:16 +0300

    part2.1 - 2.5

---
+++ Part2/part2.1_2.5/README.md
@@ -0,0 +1,10 @@
+# React + Vite
+
+How to run locally: 
+- Open Integrated Terminal at folder part2.1_2.5
+- Run  `npm install`
+- Run `npm run dev`
+- 
+Result
+<img src="./public\part2.1_2.5.png" width="800" height="500">
+
--- Part2/part2.1_2.5/src/App.jsx
+++ Part2/part2.1_2.5/src/App.jsx
@@ -1,7 +1,6 @@
 import Course from "./components/Course"
 
 const App = ({courses}) => {
-  // console.log(courses[1].parts.map(each => each.exercises).reduce((acc, curr) => acc+curr),initial)
   return (
     <>
       <Course course={courses} />
